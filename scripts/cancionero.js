ae9612dd36368ac0aebe993160e06f1b7642d274
alvarezroquebenjamin-ctrl <alvarezroquebenjamin@gmail.com>
2026-06-08 20:28:03 -0300

    correccion acordes con bajo

--- scripts/cancionero.js
+++ scripts/cancionero.js
@@ -862,14 +862,93 @@ window.canciones = {
     var getNewKey = function (oldKey, delta, targetKey) { var keyValue = getKeyByName(oldKey).value + delta; if (keyValue > 11) keyValue -= 12; else if (keyValue < 0) keyValue += 12; var i=0; if (keyValue == 0 || keyValue == 2 || keyValue == 5 || keyValue == 7 || keyValue == 10) { switch(targetKey.name) { case "LA": case "LA#": case "SI": case "DO": case "DO#": case "RE": case "RE#": case "MI": case "FA#": case "SOL": case "SOL#": for (;i<keys.length;i++) { if (keys[i].value == keyValue && keys[i].type == "S") return keys[i]; } default: for (;i<keys.length;i++) { if (keys[i].value == keyValue && keys[i].type == "F") return keys[i]; } } } else { for (;i<keys.length;i++) { if (keys[i].value == keyValue) return keys[i]; } } };
     var getDelta = function (oldIndex, newIndex) { if (oldIndex > newIndex) return 0 - (oldIndex - newIndex); else if (oldIndex < newIndex) return 0 + (newIndex - oldIndex); else return 0; };
     var transposeSong = function (target, key) { var newKey = getKeyByName(key); if (currentKey.name == newKey.name) return; var delta = getDelta(currentKey.value, newKey.value); $("span.c", target).each(function (i, el) { transposeChord(el, delta, newKey); }); currentKey = newKey; };
-    var transposeChord = function (selector, delta, targetKey) { var el = $(selector); if (!el.data("orig-block-len")) { var next = el[0].nextSibling; var spaces = 0; if (next && next.nodeType === 3) { var m = next.nodeValue.match(/^(\s+)/); if (m) spaces = m[1].length; } el.data("orig-block-len", el.text().length + spaces); } var originalBlockLen = el.data("orig-block-len"); var oldChord = el.text(); var oldChordRoot = getChordRoot(oldChord); var newChordRoot = getNewKey(oldChordRoot, delta, targetKey); var newChord = newChordRoot.name + oldChord.substr(oldChordRoot.length); var spacesNeeded = originalBlockLen - newChord.length; if (spacesNeeded < 0) spacesNeeded = 0; el.text(newChord); var next = el[0].nextSibling; if (next && next.nodeType === 3) { next.nodeValue = " ".repeat(spacesNeeded) + next.nodeValue.replace(/^\s+/, ""); } else if (spacesNeeded > 0) { el.after(document.createTextNode(" ".repeat(spacesNeeded))); } };
+    
+    var transposeChord = function (selector, delta, targetKey) { 
+        var el = $(selector); 
+        if (!el.data("orig-block-len")) { 
+            var next = el[0].nextSibling; var spaces = 0; 
+            if (next && next.nodeType === 3) { 
+                var m = next.nodeValue.match(/^(\s+)/); 
+                if (m) spaces = m[1].length; 
+            } 
+            el.data("orig-block-len", el.text().length + spaces); 
+        } 
+        
+        var originalBlockLen = el.data("orig-block-len"); 
+        var oldChord = el.text(); 
+        var newChord = "";
+
+        // TRANSPOSICIÓN DOBLE PARA BAJOS INVERTIDOS
+        if (oldChord.indexOf("/") !== -1) {
+            var parts = oldChord.split("/");
+            var mainChord = parts[0];
+            var bassChord = parts[1];
+
+            var mainRoot = getChordRoot(mainChord);
+            var newMainRoot = getNewKey(mainRoot, delta, targetKey);
+            var newMain = newMainRoot.name + mainChord.substr(mainRoot.length);
+
+            var bassRoot = getChordRoot(bassChord);
+            var newBassRoot = getNewKey(bassRoot, delta, targetKey);
+            var newBass = newBassRoot.name + bassChord.substr(bassRoot.length);
+
+            newChord = newMain + "/" + newBass;
+        } else {
+            var oldChordRoot = getChordRoot(oldChord); 
+            var newChordRoot = getNewKey(oldChordRoot, delta, targetKey); 
+            newChord = newChordRoot.name + oldChord.substr(oldChordRoot.length); 
+        }
+
+        var spacesNeeded = originalBlockLen - newChord.length; 
+        if (spacesNeeded < 0) spacesNeeded = 0; 
+        el.text(newChord); 
+        var next = el[0].nextSibling; 
+        if (next && next.nodeType === 3) { 
+            next.nodeValue = " ".repeat(spacesNeeded) + next.nodeValue.replace(/^\s+/, ""); 
+        } else if (spacesNeeded > 0) { 
+            el.after(document.createTextNode(" ".repeat(spacesNeeded))); 
+        } 
+    };
+    
     var wrapChords = function (input) { return input.replace(opts.chordReplaceRegex, "<span class='c'>$1</span>"); };
-    return $(this).each(function() { var startKey = $(this).attr("data-key"); if (!startKey || $.trim(startKey) == "") startKey = opts.key; if (!startKey || $.trim(startKey) == "") return this; currentKey = getKeyByName(startKey); var keyLinks = []; $(keys).each(function(i, key) { if (currentKey.name == key.name) keyLinks.push("<a href='#' class='selected'>" + key.name + "</a>"); else keyLinks.push("<a href='#'>" + key.name + "</a>"); }); var $this = $(this); var keysHtml = $("<div class='transpose-keys justify-content-md-center' style='margin-bottom:10px;'></div>"); keysHtml.html(keyLinks.join("")); $("a", keysHtml).click(function(e) { e.preventDefault(); transposeSong($this, $(this).html()); $(".transpose-keys a").removeClass("selected"); $(this).addClass("selected"); return false; }); $(this).before(keysHtml); var output = []; var lines = $(this).html().split("\n"); var line; for (var i = 0; i < lines.length; i++) { line = lines[i]; if (isChordLine(line)) output.push("<span>" + wrapChords(line) + "</span>"); else output.push("<span>" + line + "</span>"); }; $(this).html(output.join("\n")); });
+    
+    return $(this).each(function() { 
+        var startKey = $(this).attr("data-key"); 
+        if (!startKey || $.trim(startKey) == "") startKey = opts.key; 
+        if (!startKey || $.trim(startKey) == "") return this; 
+        currentKey = getKeyByName(startKey); 
+        var keyLinks = []; 
+        $(keys).each(function(i, key) { 
+            if (currentKey.name == key.name) keyLinks.push("<a href='#' class='selected'>" + key.name + "</a>"); 
+            else keyLinks.push("<a href='#'>" + key.name + "</a>"); 
+        }); 
+        var $this = $(this); 
+        var keysHtml = $("<div class='transpose-keys justify-content-md-center' style='margin-bottom:10px;'></div>"); 
+        keysHtml.html(keyLinks.join("")); 
+        $("a", keysHtml).click(function(e) { 
+            e.preventDefault(); 
+            transposeSong($this, $(this).html()); 
+            $(".transpose-keys a").removeClass("selected"); 
+            $(this).addClass("selected"); 
+            return false; 
+        }); 
+        $(this).before(keysHtml); 
+        var output = []; 
+        var lines = $(this).html().split("\n"); 
+        var line; 
+        for (var i = 0; i < lines.length; i++) { 
+            line = lines[i]; 
+            if (isChordLine(line)) output.push("<span>" + wrapChords(line) + "</span>"); 
+            else output.push("<span>" + line + "</span>"); 
+        }; 
+        $(this).html(output.join("\n")); 
+    });
   };
+  
   $.fn.transpose.defaults = { 
     chordRegex: /^(\bDO|\bRE|\bMI|\bFA|\bSOL|\bLA|\bSI)[b\#]?(2|4|5|6|7|9|11|13|6\/9|6\(9\)|7\-5|7\-9|7\#5|7\#9|7\+5|7\+9|7b5|7b9|7sus2|7sus4|9sus4|add2|add4|add9|aug|°|dim|Ø|dim7|mb5|m7b5|m\/maj7|mMaj7|m6|m7|m9|m11|m13|m79|maj7|maj9|maj11|maj13|majb5|m|sus|sus2|sus4|\([0-9b\#\+\-\/]+\)|\+)*(\/(DO|RE|MI|FA|SOL|LA|SI)[b\#]?)*$/, 
     chordReplaceRegex: /((\bDO|\bRE|\bMI|\bFA|\bSOL|\bLA|\bSI)[b\#]?(2|4|5|6|7|9|11|13|6\/9|6\(9\)|7\-5|7\-9|7\#5|7\#9|7\+5|7\+9|7b5|7b9|7sus2|7sus4|9sus4|add2|add4|add9|aug|°|dim|Ø|dim7|mb5|m7b5|m\/maj7|mMaj7|m6|m7|m9|m11|m13|m79|maj7|maj9|maj11|maj13|majb5|m|sus|sus2|sus4|\([0-9b\#\+\-\/]+\)|\+)*(\/(DO|RE|MI|FA|SOL|LA|SI)[b\#]?)*)/g 
-};
+  };
   
   $(function() { 
       var targets = $("#letra, .zona-letra"); 
